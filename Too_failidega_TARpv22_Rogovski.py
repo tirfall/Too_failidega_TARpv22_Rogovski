4e64af366035f188017da592da5f860812ddc1d6
opilane <opilane@HP-CZC2349HTB>
2023-02-20 14:06:43 +0200

    Add project files.

---
+++ Too_failidega_TARpv22_Rogovski.py
@@ -0,0 +1,19 @@
+﻿from funktsioonid import *
+laused=[]
+
+while True:
+    v=int(input("1 - loeme failist\n2 - salvestame failisse\n3 - Tekst helisse\n"))
+    if v==1:
+        laused=Loe_failist("laused.txt")
+        for line in laused:
+            print(line)
+    elif v==2:
+        line=input("Lisa lause")
+        laused.append(line)
+        sal_failisse("laused.txt", laused)
+    elif v==3:
+        text=" "
+        for line in laused:
+            text=text+line
+        #ind=int(input("Number: "))
+        Heli(text,"et")
---
+++ funktsioonid.py
@@ -0,0 +1,30 @@
+﻿def Loe_failist(fail:str)->list:
+    """
+    Me loeme info failist
+    """
+    f=open(fail, "r", encoding="utf-8-sig")
+    jarjend=[]
+    for rida in f:
+        jarjend.append(rida.strip())
+    f.close()
+    return jarjend
+
+def sal_failisse(fail:str, jarjend:list):
+    """
+    Me salvestame info failisse
+    """
+    f=open(fail, "w", encoding="utf-8-sig")
+    for line in jarjend:
+        f.write(line+"\n")
+    f.close()
+
+from gtts import gTTS
+import os
+
+
+def Heli(text:str, keel:str):
+    """
+    Helime failis
+    """
+    obj=gTTS(text=text, lang=keel, slow=False).save("heli.mp3")
+    os.system("heli.mp3")
